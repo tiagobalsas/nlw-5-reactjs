89f95da2d2e84698f1ca05393177fe8b465655d5
Tiago Santos <tiago881@gmail.com>
2022-06-16 18:11:47 -0300

    feat(footer):add buttons

--- podcastr/src/components/Player/index.tsx
+++ podcastr/src/components/Player/index.tsx
@@ -12,7 +12,35 @@ export function Player() {
         <strong>Selecione um podcast para ouvir</strong>
       </div>
 
-      <footer></footer>
+      <footer>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.emptySlider} />
+          <span>00:00</span>
+        </div>
+
+        <div className={styles.buttons}>
+          <button type='button'>
+            <img src='/shuffle.svg' alt='Embaralhar' />
+          </button>
+
+          <button type='button'>
+            <img src='/play-previus.svg' alt='Tocar anterior' />
+          </button>
+
+          <button type='button' className={styles.playButton}>
+            <img src='/play.svg' alt='Tocar' />
+          </button>
+
+          <button type='button'>
+            <img src='/play-next.svg' alt='Tocar próxima' />
+          </button>
+
+          <button type='button'>
+            <img src='/repeat.svg' alt='Repetir' />
+          </button>
+        </div>
+      </footer>
     </div>
   );
 }
