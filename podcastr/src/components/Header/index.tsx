a163faab193dbdd4b45d7716650fa31344286c1c
Tiago Santos <tiago881@gmail.com>
2022-06-16 16:38:46 -0300

    feat(date):add current date

--- podcastr/src/components/Header/index.tsx
+++ podcastr/src/components/Header/index.tsx
@@ -1,9 +1,17 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+import styles from './styles.module.scss';
+
 export function Header() {
+  const currentDate = format(new Date(), 'EEEE, d, MMMM', {
+    locale: ptBR
+  });
+
   return (
-    <header>
+    <header className={styles.headerContainer}>
       <img src='/logo.svg' alt='Podcastr' />
       <p>O melhor para você ouvir, sempre</p>
-      <span>Qui, 16 Junho</span>
+      <span>{currentDate}</span>
     </header>
   );
 }
