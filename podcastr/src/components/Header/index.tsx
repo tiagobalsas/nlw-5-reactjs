9a70ce9a2754116e98bd9ca804e1c7bb27d93814
Tiago Santos <tiago881@gmail.com>
2022-06-16 15:52:17 -0300

    feat: add Header component

---
+++ podcastr/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <img src='/logo.svg' alt='Podcastr' />
+      <p>O melhor para você ouvir, sempre</p>
+      <span>Qui, 16 Junho</span>
+    </header>
+  );
+}
--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { Header } from '../components/Header/index';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <Header />
+  );
 }
