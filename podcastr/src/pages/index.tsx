0e7478830094b3dd35afe2f3c8af783c4bd761d6
Tiago Santos <tiago881@gmail.com>
2022-06-17 11:37:30 -0300

    feat:add formats date and time

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { GetStaticProps } from 'next';
 import { api } from '../services/api';
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
   members: string;
+  published_at: string;
 };
 
 type HomeProps = {
@@ -29,9 +33,27 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
   return {
     props: {
-      episodes: data,
+      episodes: episodes,
     },
     revalidate: 60 * 60 * 8,
   };
