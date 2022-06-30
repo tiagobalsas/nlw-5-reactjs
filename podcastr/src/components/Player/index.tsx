be89ad9568ad6435bb84017b64a70bdae52b7b4a
Tiago Santos <tiago881@gmail.com>
2022-06-30 08:29:35 -0300

    fix: opacity button

--- podcastr/src/components/Player/index.tsx
+++ podcastr/src/components/Player/index.tsx
@@ -36,7 +36,7 @@ export function Player() {
         </div>
       )}
 
-      <footer className={styles.empty}>
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
